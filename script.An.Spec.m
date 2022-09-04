85b89b7b561855643f0029c581d45b6dbc7e6771
UT57 <EgorkaEgorovich@yandex.ru>
2022-09-04 19:42:38 +0300

    clean

--- script.An.Spec.m
+++ script.An.Spec.m
@@ -2,33 +2,35 @@
 clear
 Nx=201;
 Ny=201;
-tic
 
+
+tic
 for iy=1:Ny
      for ix=1:Nx
-         ck=clock;
-         
-fnm=['C:\Users\Егор\Documents\Holography\21.10.22Fn0.7Holo\Holo\ScanX' num2str(ix-1,'%03d') 'Y' num2str(iy-1,'%03d') '.txt'];
-         a=caseread(fnm);
-         x=str2num(a(82:size(a,1),:));
-         for ii=1:length(x(:,1)) %ii=1:floor(length(x(:,1))/5)
-             %downscaling from 25k to 5k
-             %wfm(ii,ix,iy)=x(ii*5,2);
-             wfm(ii,ix,iy)=x(ii,2); %signal
-         end
-         rt=etime(clock,ck)*(1-(iy-1+ix/Nx)/Ny)*Nx*Ny;
-%%fprintf(['scat calc 'num2str((iyc-1+(iys-1+ixs/Nx)/Ny)/Ny*100,'%04.3f') '%% done,' 'Remaining time = ' ;num2str(rt/60,'%04.1f'),' mins\n'])
-% fprintf(['scat calc 'num2str((iys-1+ixs/Nx)/Ny*100,'%04.3f') '%% done, ','Remaining time =';num2str(rt/60,'%04.1f'),' mins\n'])
-         fprintf(['files loading process' num2str((iy-1+ix/Nx)/Ny*100,'%04.2f') '%% done ','Remaining time =',num2str(rt/60,'%04.1f'),' mins\n'])
+         ck=clock;        
+        fnm=['C:\Users\Егор\Documents\Holography\21.10.22Fn0.7Holo\Holo\ScanX', ...
+        num2str(ix-1,'%03d') 'Y' num2str(iy-1,'%03d') '.txt'];
+        a=caseread(fnm);
+        x=str2num(a(82:size(a,1),:));
+        for ii=1:length(x(:,1)) 
+            %ii=1:floor(length(x(:,1))/5)
+            %downscaling from 25k to 5k
+            %wfm(ii,ix,iy)=x(ii*5,2);
+            wfm(ii,ix,iy)=x(ii,2);      %сигнал
+        end
+        rt=etime(clock,ck)*(1-(iy-1+ix/Nx)/Ny)*Nx*Ny;
+        fprintf(['files loading process' num2str((iy-1+ix/Nx)/Ny*100,'%04.2f'), ...
+        '%% done ','Remaining time =',num2str(rt/60,'%04.1f'),' mins\n'])
      end
 end
 toc
-  for ii=1:length(x(:,1))
-     tt(ii)=x(ii,1); %time
+
+for ii=1:length(x(:,1))
+     tt(ii)=x(ii,1);    %время 
 end
 save loaded_wfms_inphase.mat -v7.3
 Table = permute(wfm,[3,2,1]);
-%    pcolor(Table(:,:,1));
+
 
 
 %% Обработка
@@ -67,14 +69,18 @@ X_max = Nx*dx; Y_max = Ny*dy;
 x = [-(X_max-dx)/2:dx:(X_max-dx)/2]; y = x;
 [X,Y] = meshgrid(x,y);
 
+
 % Сетка [kx,ky]:
 dk_x = 2*pi/X_max; dk_y = dk_x;
 Kx_m = 2*pi/dx; Ky_m = Kx_m;
 kx = [-(Kx_m-dk_x)/2:dk_x:(Kx_m-dk_x)/2]; ky = kx;
 [Kx,Ky] = meshgrid(kx,ky);
 
+
 IST = zeros(Nx,Ny);
 IST(Kx.^2 + Ky.^2 <= kk.^2) = 1; 
+
+
 %Метод У.С.:
 %Прямое пр:
 S_new = fftshift(fft2(Res)); %conj(Res)
@@ -89,12 +95,17 @@ for ix = 1:Nx
     end
 end
 
-prop = exp(1i*Zh*kz);%пропагатор 
+
+prop = exp(1i*Zh*kz);   %пропагатор 
 S_obr = S_new.*prop;
+
 
 %Обратное БПФ:     
 Res_obr_test =(ifft2(S_obr));
-%pcolor(abs(Res_obr));
+nexttitle
+pcolor(abs(Res_obr));
+title('Результат')
+
 %Отклонение :
 count = 0;
 for ix = 1:Nx
@@ -105,6 +116,7 @@ end
 pla(ind) = (count/Nx/(Nx-1));
 ind = ind + 1;
 end
+nexttitle
 plot(pla);
 title('Стандартное отклонение в условтях линейного увеличения температуры')
 xlabel('Разница температур в начале и конце измерений ') 
